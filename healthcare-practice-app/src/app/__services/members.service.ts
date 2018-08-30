17659cae110201e0891a558153eae9c6fe0d4ee2
Herod, Kathryn <codingkat7@gmail.com>
2018-08-29 19:26:27 -0500

    remove mockdata folder

--- healthcare-practice-app/src/app/__services/members.service.ts
+++ healthcare-practice-app/src/app/__services/members.service.ts
@@ -11,6 +11,7 @@ export class MembersService {
   constructor(private http: HttpClient) {}
   members: any;
 
+  //need to add model for members
   getMembersList(): Observable<any> {
     return this.http
       .get('http://localhost:3000/api/members')
